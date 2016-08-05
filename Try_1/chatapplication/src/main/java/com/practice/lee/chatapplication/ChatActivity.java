97a7cc1a41b9bd8b2917ce16ab3f8ddaadad7ecd
LHM0428 <lhm3031@hanmail.net>
2016-08-05 17:15:40 +0900

    finish my first example 'chatting'

--- Try_1/chatapplication/src/main/java/com/practice/lee/chatapplication/ChatActivity.java
+++ Try_1/chatapplication/src/main/java/com/practice/lee/chatapplication/ChatActivity.java
@@ -1,10 +1,13 @@
 package com.practice.lee.chatapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -37,15 +40,28 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if(v.equals(bt_send)){
-            String inputText = Input_Message.getText().toString();
-            Input_Message.setText("");
-            String answer;
+        final int marginSize = getResources().getDimensionPixelSize(R.dimen.message_margin);
+        TextView userMessage = new TextView(this);
+        LinearLayout.LayoutParams userMssageLayout = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        final TextView cMessage = new TextView(this);
+        final LinearLayout.LayoutParams cMssageLayout = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
-            TextView userMessage = new TextView(this);
+        if(v.equals(bt_send)){
+            String answer;
+            String inputText = Input_Message.getText().toString();
+
             userMessage.setText(inputText);
-            userMessage.setGravity(Gravity.END);
-            message_log.addView(userMessage, LinearLayout.LayoutParams.WRAP_CONTENT);
+            userMssageLayout.gravity = Gravity.END;
+            userMssageLayout.setMargins(0, marginSize,0,marginSize);
+
+            userMessage.setBackgroundResource(R.drawable.user_message);
+            userMessage.setTextColor(Color.WHITE);
+            message_log.addView(userMessage, LinearLayout.LayoutParams.WRAP_CONTENT, userMssageLayout);
+
+            Input_Message.setText("");
+            TranslateAnimation userMessageAnimation = new TranslateAnimation(message_log.getWidth(),0,0,0);
+            userMessageAnimation.setDuration(500);
+            userMessage.startAnimation(userMessageAnimation);
 
             if(inputText.contains("안녕")){
                 answer="안녕하세요";
@@ -72,11 +88,36 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
                 answer = "할말 없음.";
             }
 
-            TextView cMessage = new TextView(this);
             cMessage.setText(answer);
-            cMessage.setGravity(Gravity.START);
-            message_log.addView(cMessage,LinearLayout.LayoutParams.WRAP_CONTENT);
-            sc_text.fullScroll(View.FOCUS_DOWN);
+            cMessage.setBackgroundResource(R.drawable.cpu_message);
+            cMessage.setTextColor(Color.WHITE);
+            userMessageAnimation.setAnimationListener(new Animation.AnimationListener(){
+                @Override
+                public void onAnimationStart(Animation animation) {}
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    sc_text.fullScroll(View.FOCUS_DOWN);
+                    cMssageLayout.gravity = Gravity.START;
+                    cMssageLayout.setMargins(marginSize,marginSize,marginSize,marginSize);
+                    message_log.addView(cMessage, LinearLayout.LayoutParams.WRAP_CONTENT, cMssageLayout);
+                    TranslateAnimation cMessageAnimation = new TranslateAnimation(-message_log.getWidth(),0,0,0);
+                    cMessageAnimation.setDuration(500);
+                    cMessage.startAnimation(cMessageAnimation);
+                    cMessageAnimation.setAnimationListener(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {                     }
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+                            sc_text.fullScroll(View.FOCUS_DOWN);
+                        }
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {                     }
+                    });
+                }
+                @Override
+                public void onAnimationRepeat(Animation animation) {}
+            });
+
         }//bt_send
     }
 }
